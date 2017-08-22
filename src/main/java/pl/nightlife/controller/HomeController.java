dd001021c0c434ceec3314b4a28ca166bdbcbfdd
Przemek <magiera.przemyslaw@gmail.com>
2017-08-22 15:33:29 +0200

    first setup

---
+++ src/main/java/pl/nightlife/controller/HomeController.java
@@ -0,0 +1,21 @@
+package pl.nightlife.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Przemek on 22.08.2017.
+ */
+
+@Controller
+@RequestMapping("/")
+public class HomeController
+{
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String getAdminMainPage(Model model)
+    {
+        return "home/mainPage";
+    }
+}
